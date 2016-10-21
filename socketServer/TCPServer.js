d81f5b157478bf94127c9367fbe73a3d0c3cd6e4
Addler Huang <addler@sina.cn>
2016-10-21 10:40:41 +0800

    handle multi JSON in one line

--- socketServer/TCPServer.js
+++ socketServer/TCPServer.js
@@ -9,23 +9,38 @@ var logger = require('../utils/log').getLogger();
 
 
 var recvMessage = function(data){
-    var message = null;
+    var message = [];
     try {
-        message = JSON.parse(data.toString('utf8'));
+        message.push(JSON.parse(data.toString('utf8')));
     } catch (e) {
         console.log(data.toString('utf8'));
         console.log(e);
+        //handle multi JSON date in one line, like /test/message.json
+        var string = data.toString('utf8');
+        try {
+            var index = null;
+            while(index = string.match('\}[ ]*\{')){
+                var v = string.substring(0, index.index + 1);
+                string = string.substring(index.index + index[0].length - 1);
+                message.push(JSON.parse(v));
+            }
+            message.push(JSON.parse(string));
+        }catch(e){
+            console.log(` --> ${string}`);
+            console.log(message);
+            console.log(e);
+        }
     } finally {
 
     }
-    if (message) {
-    var ds = message.ds;
+    _.each(message, (m) => {
+        var ds = m.ds;
         _.each(ds, (obj) => {
             var name = user.getUserName(obj.da);
         // console.log(` RECV mac : [${obj.da}], user : [${name}]`);
             location.saveLocationData(obj.da, obj);
-        })
-    }
+        });
+    });
     // console.log(message);
 }
 var connection = function(socket) {
